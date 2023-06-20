8f44499b87dddf4b4a0d0ad5120e871806032d26
Tran Diep <dogtortron@github.com>
2023-06-20 00:45:04 -0600

    delete iframe bc it's not working in React

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './about.css';
+import '../app.css';
 
 export function About(props) {
   const [birdUrl, setImageUrl] = React.useState('');
@@ -10,10 +11,8 @@ export function About(props) {
     // fetch('/api/birds')
       .then((response) => response.json())
       .then((birdInfo) => {
-        console.log(birdInfo)
         // const birdUrl = birdInfo[0];
         const birdObj = birdInfo[0];
-        console.log(birdObj.download_url)
         const birdUrl = birdObj.download_url;
         //const birdUrl = birdObj.urls.regular;
         //const birdDescr = birdObj.description;
@@ -23,14 +22,13 @@ export function About(props) {
   }, []);
 
   return (
-    <main className='container-fluid text-center'>
-      <div>About displayed here</div>
+    <main className='container-fluid'>
       <div className="container-fluid">
         <div className="about">
         <h2>About</h2>
         <p>How to play: Click "Sing, Billy" and Billy will sing for you. Try your best to remember his notes and how many seconds he pauses in between. Right now there are 4 notes and either 1 or 2 seconds.</p>
         <p>The inspiration of this game came from a talk titled <a href="https://www.churchofjesuschrist.org/study/general-conference/1973/04/yellow-canaries-with-gray-on-their-wings?lang=eng">Yellow Canaries with Gray on Their Wings</a>, given by Thomas S. Monson in 1973.</p>
-        <iframe id="myiframe" src="https://www.youtube.com/embed/W-vq72HOsOg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        {/* <frameborder id="myiframe" src="https://www.youtube.com/embed/W-vq72HOsOg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></frameborder> */}
         </div>
         <div className="birdOfTheDay">
         <section>
